479920f0550bb48c27fbe7e49c90fe89e059fbd1
aswinss18 <aswinartography18@gmail.com>
2024-11-01 19:47:57 +0530

    Home page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Container } from "@mui/material"
 import Navbar from "./components/ui/Navbar"
+import Home from "./components/pages/Home"
 
 
 function App() {
 
 
   return (
-    <Container sx={{ backgroundColor: 'primary.main' }} maxWidth={false}  >
+    <Container sx={{ backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center' }} maxWidth={false}  >
       <Navbar />
+      <Home />
     </Container>
   )
 }
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -4,12 +4,14 @@ import React from 'react'
 import Logo from './Logo'
 import NavLink from './NavLink'
 import Button from './Button'
+import { useTheme } from '@emotion/react'
 
 export default function Navbar() {
+    const theme = useTheme()
     return (
 
         <Container maxWidth='lg' sx={{
-            display: 'flex', alignItems: 'center', justifyContent: "space-between", height: '64px', width: '100%'
+            display: 'flex', alignItems: 'center', justifyContent: "space-between", height: '64px', width: '100%', position: 'fixed', zIndex: 50, backgroundColor: theme.palette.primary.main
         }}>
             <Logo />
             <Box sx={{ display: 'flex', justifyContent: "flex-between", gap: '70px ', alignItems: 'center' }}>
